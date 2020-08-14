2eb5948430008ec03fbb094ddf0112af9831ef18
Jonathan Zhou <jzhou62@uw.edu>
2020-08-13 23:48:03 -0700

    Added some join functionality

--- js/test.js
+++ js/test.js
@@ -5,3 +5,9 @@ function initialize() {
         alert('My peer ID is: ' + id);
     });
 }
+
+function joint() {
+    var dID = prompt('Input your host\'s ID:');
+    var conn = peer.connect(dID);
+    alert('made it ');
+}
