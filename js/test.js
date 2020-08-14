458b3cc5ec8353c84d5eeee5e3ab0b1a71955bf9
Jonathan Zhou <jzhou62@uw.edu>
2020-08-14 00:41:09 -0700

    Added chatting test functionality

--- js/test.js
+++ js/test.js
@@ -6,21 +6,19 @@ function initialize() {
     });
     peer.on('connection', function(conn) {
         alert('We have liftoff');
+        conn.send('Hello World');
     });
 }
 
 function joint() {
     var dID = prompt('Input your host\'s ID:');
     var conn = peer.connect(dID);
-    /*conn.on('open', function() {
-        // Receive messages
-        conn.on('data', function(data) {
-          console.log('Received', data);
-        });
+
+    conn.on('data', function(data) {
+        alert(data);
+        console.log(data);
+    });
       
-        // Send messages
-        conn.send('Hello!');
-    });*/
     /*if(conn.open) {   
         alert('made it ');
     } else {
