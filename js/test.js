a14cd2bc4fdf78ece9e7cf089a4eea18431d5bea
Jonathan Zhou <jzhou62@uw.edu>
2020-08-14 11:26:01 -0700

    Reworked to use self-hosted Heroku server

--- js/test.js
+++ js/test.js
@@ -1,10 +1,38 @@
 var peer = new Peer();
+var conn = null;
+var dID = null;
+
+function server() {
+    peer = new Peer('', {secure:true, host:'happylifegame.herokuapp.com', port:443});
+    peer.on('open', function(id) {
+        dID = id;
+    });
+    peer.on('error', function(err) {
+        alert('' + err);
+    });
+    function ping() {
+        console.log(peer)
+        peer.socket.send({
+            type: 'ping'
+        })
+        setTimeout(ping, 16000)
+    }
+    ping()
+}
 
 function initialize() {
-    peer.on('open', function(id) {
-        document.getElementById("hostID").innerHTML = id;
+    server();
+    peer.on('open', function() {
+        document.getElementById("hostID").innerHTML = dID;
     });
-    peer.on('connection', function(conn) {
+    alert(dID);
+    peer.on('connection', function(incoming) {
+        /*if(conn) {
+            incoming.close();
+            alert('connection is closed');
+            return;
+        }*/
+        conn = incoming;
         alert('We have liftoff');
         var phrase = prompt('Type your message here');
         conn.on('open', function() {
@@ -15,12 +43,18 @@ function initialize() {
 }
 
 function joint() {
-    var dID = prompt('Input your host\'s ID:');
-    var conn = peer.connect(dID);
-    conn.on('open', function() {
-        conn.on('data', function(data) {
-            alert('Received this message: ' + data);
-            console.log(data);
+    server();
+    peer.on('open', function(){
+        alert('I am here at least');
+        dID = prompt('Input your host\'s ID:');
+        conn = peer.connect(dID);
+        alert(dID);
+        conn.on('open', function() {
+            alert('Conn working');
+            conn.on('data', function(data) {
+                alert('Received this message: ' + data);
+                console.log(data);
+            });
         });
     });
 }
@@ -37,6 +71,7 @@ function readySwitch() {
     } else{
         testelm.value = "Not Ready";
         alert('We not here');
+
         conn.on('open', function() {
             alert('Test2');
             conn.send(false);
