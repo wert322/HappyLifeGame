5d4ff46a2336b1cf05a4be644336582ca4a52b90
Jonathan Zhou <jzhou62@uw.edu>
2020-08-14 00:52:18 -0700

    Added messaging functionality

--- js/test.js
+++ js/test.js
@@ -6,7 +6,10 @@ function initialize() {
     });
     peer.on('connection', function(conn) {
         alert('We have liftoff');
-        conn.send('Hello World');
+        conn.on('open', function() {
+            alert('The message was sent');
+            conn.send('Hello World');
+        });
     });
 }
 
@@ -14,10 +17,13 @@ function joint() {
     var dID = prompt('Input your host\'s ID:');
     var conn = peer.connect(dID);
 
-    conn.on('data', function(data) {
-        alert(data);
-        console.log(data);
+    conn.on('open', function() {
+        conn.on('data', function(data) {
+            alert('Got something');
+            console.log(data);
+        });
     });
+    
       
     /*if(conn.open) {   
         alert('made it ');
