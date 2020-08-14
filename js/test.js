8a14de8118f154edb8bee76add5c115e7e91c8bb
Jonathan Zhou <jzhou62@uw.edu>
2020-08-14 00:29:51 -0700

    Established connection conventions

--- js/test.js
+++ js/test.js
@@ -4,10 +4,26 @@ function initialize() {
     peer.on('open', function(id) {
         alert('My peer ID is: ' + id);
     });
+    peer.on('connection', function(conn) {
+        alert('We have liftoff');
+    });
 }
 
 function joint() {
     var dID = prompt('Input your host\'s ID:');
     var conn = peer.connect(dID);
-    alert('made it ');
+    /*conn.on('open', function() {
+        // Receive messages
+        conn.on('data', function(data) {
+          console.log('Received', data);
+        });
+      
+        // Send messages
+        conn.send('Hello!');
+    });*/
+    /*if(conn.open) {   
+        alert('made it ');
+    } else {
+        alert('nope');
+    }*/
 }
