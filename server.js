40a6bbf1c965b9874ed5608c78a8ff440cd5cea7
Jonathan Zhou <jzhou62@uw.edu>
2020-08-15 20:32:54 -0700

    Added array to keep track of active rooms and remove them automatically when emptied

--- server.js
+++ server.js
@@ -12,13 +12,19 @@ const io = socketio(server);
 app.use(express.static(path.join(__dirname, 'public')));
 
 const botName = 'Server';
+var roomList = [];
 
 // Run when client connects
 io.on('connection', socket => {
     socket.on('joinRoom', ({ username, room}) => {
         const user = userJoin(socket.id, username, room);
 
-        socket.join(user.room);
+        socket.join(user.room); 
+
+        // Checks if this room has already been made, and adds to the list if not
+        if (roomList.indexOf(room) === -1) {
+            roomList.push(room);
+        }
 
         // Welcome current user
         socket.emit('message', formatMessage(botName, 'Welcome!'));
@@ -43,10 +49,17 @@ io.on('connection', socket => {
     // Runs when client disconnects
     socket.on('disconnect', () => {
         const user = userLeave(socket.id);
-
+        
+        // Removes room from list if it's no longer populated/active
+        if (getRoomUsers(user.room).length === 0) {
+            var indexPlace = roomList.indexOf(user.room);
+            if (indexPlace > -1) {
+                roomList.splice(indexPlace, 1);
+            };
+        };
+        
         if(user) {
             io.to(user.room).emit('message', formatMessage(botName, `${user.username} has left the chat.`));
-
             // Send users and room info
             io.to(user.room).emit('roomUsers', {
                 room: user.room,
