fa1a1d7e45940762a66cbfc636515aa363c7381e
Jonathan Zhou <jzhou62@uw.edu>
2020-08-26 00:26:05 -0700

    Testing DB access

--- server.js
+++ server.js
@@ -16,6 +16,13 @@ var roomList = [];
 
 // Run when client connects
 io.on('connection', socket => {
+    socket.on('testDB', (filler) => {
+        const {Client} = require('pg');
+        const client = new Client({
+            connectionString: process.env.DATABASE_URL,
+        })
+        client.connect();
+    });
 
     // Checks if the room is full and responds with an appropriate signaling event
     socket.on('joinTest', ({room}) => {
